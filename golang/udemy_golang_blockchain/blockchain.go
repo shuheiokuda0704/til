53c45c085b935bcb2885f5d4fe96bdd00464c8de
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-05-07 22:21:43 +0900

    Add struct Block and Print() method

--- golang/udemy_golang_blockchain/blockchain.go
+++ golang/udemy_golang_blockchain/blockchain.go
@@ -3,7 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp      %d\n", b.timestamp)
+	fmt.Printf("nonce          %d\n", b.nonce)
+	fmt.Printf("previousHash   %s\n", b.previousHash)
+	fmt.Printf("transactions   %s\n", b.transactions)
+}
 
 func init() {
 	log.SetPrefix("Blockchain: ")
@@ -11,4 +34,7 @@ func init() {
 func main() {
 	log.Println("test")
 	fmt.Println("hello")
+	b := NewBlock(0, "init hash")
+	fmt.Println(b)
+	b.Print()
 }
