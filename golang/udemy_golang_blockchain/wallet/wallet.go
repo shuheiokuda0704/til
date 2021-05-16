17c6bf5cce1a5af185d84dcf4526f28210307351
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-05-17 05:40:00 +0900

    Add commets for generating the blockchain_address

--- golang/udemy_golang_blockchain/wallet/wallet.go
+++ golang/udemy_golang_blockchain/wallet/wallet.go
@@ -22,6 +22,14 @@ func NewWallet() *Wallet {
 	w.privateKey = privateKey
 	w.publicKey = &w.privateKey.PublicKey
 
+	// 2. Perform SHA-256 hashing on the public key (32 bytes)
+	// 3. Perform RIPEMD-160 hashing on the result of SHA-256 (20 bytes)
+	// 4. Add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
+	// 5. Perform SHA-256 hash on the extended RIMEMD-160 result
+	// 6. Perform SHA-256 hash on the result of the previous SHA-256 hash
+	// 7. Take the first 4 bytes of the second SHA-256 hash for checksum
+	// 8. Add the 4 checksum bytes from 7 at the end of extended RIMEMD-160 hash from 4 (25 bytess)
+	// 9. Convert the result from a byte string into base58
 	return w
 }
 
