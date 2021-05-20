84ef09f6d6a1f5f9eca7144e6966fb0760bba816
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-05-20 23:17:03 +0900

    Initiate Blockchain Server

---
+++ golang/udemy_golang_blockchain/blockchain_server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	port := flag.Uint("port", 5000, "TCP Port Number for Blockchain Server")
+	flag.Parse()
+	fmt.Println(*port)
+}
