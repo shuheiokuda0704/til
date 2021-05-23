644b9c547f49ff21ec643e64c4ce7665ac39299d
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-05-23 14:53:42 +0900

    Add GetChain API

--- golang/udemy_golang_blockchain/blockchain_server/blockchain_server.go
+++ golang/udemy_golang_blockchain/blockchain_server/blockchain_server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"goblockchain/block"
+	"goblockchain/wallet"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 )
+
+var cache map[string]*block.Blockchain = make(map[string]*block.Blockchain)
 
 type BlockchainServer struct {
 	port uint16
@@ -19,11 +23,34 @@ func (bcs *BlockchainServer) Port() uint16 {
 	return bcs.port
 }
 
+func (bcs *BlockchainServer) GetBlockchain() *block.Blockchain {
+	bc, ok := cache["blockchain"]
+	if !ok {
+		minersWallet := wallet.NewWallet()
+		bc = block.NewBlockchain(minersWallet.BlockchainAddress(), bcs.Port())
+		cache["blockchain"] = bc
+		log.Printf("private_key        %v", minersWallet.PrivateKeyStr())
+		log.Printf("public_key         %v", minersWallet.PublicKeyStr())
+		log.Printf("blockchain_address %v", minersWallet.BlockchainAddress())
+	}
+	return bc
+}
+
 func HelloWorld(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, Wolrd")
 }
 
+func (bcs *BlockchainServer) GetChain(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		w.Header().Add("Content-type", "application/json")
+		bc := bcs.GetBlockchain()
+		m, _ := bc.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	}
+}
+
 func (bcs *BlockchainServer) Run() {
-	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/", bcs.GetChain)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcs.Port())), nil))
 }
