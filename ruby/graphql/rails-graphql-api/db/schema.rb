4fafb80e970a914fc3a439fa67a0772580281b27
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-10-17 18:36:53 +0900

    Add null: false

--- ruby/graphql/rails-graphql-api/db/schema.rb
+++ ruby/graphql/rails-graphql-api/db/schema.rb
@@ -17,15 +17,15 @@ ActiveRecord::Schema.define(version: 2021_10_17_093121) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "post_id", null: false
-    t.text "body"
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
