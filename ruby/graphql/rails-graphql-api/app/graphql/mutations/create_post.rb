19cc82bd6b0dcaed2b3db5ea8b47f841afff03d9
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-10-17 22:31:41 +0900

    Add create_post mutation

--- ruby/graphql/rails-graphql-api/app/graphql/mutations/create_post.rb
+++ ruby/graphql/rails-graphql-api/app/graphql/mutations/create_post.rb
@@ -1,14 +1,13 @@
 module Mutations
   class CreatePost < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    field :post, Types::PostType, null: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :body, String, required: true
+    argument :title, String, required: true
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(**params)
+      post = Post.create!(params)
+      { post: post }
+    end
   end
 end
