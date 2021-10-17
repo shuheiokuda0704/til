dd0964f94e175ed15e3f7dfbf1ed883d33bf916b
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-10-17 22:25:09 +0900

    Use resolver

--- ruby/graphql/rails-graphql-api/app/graphql/types/query_type.rb
+++ ruby/graphql/rails-graphql-api/app/graphql/types/query_type.rb
@@ -7,13 +7,6 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :post, Types::PostType, null: false do
-      description 'Find a post by ID'
-      argument :id, ID, required: true
-    end
-
-    def post (id:)
-      Post.find(id)
-    end
+    field :post, resolver: Resolvers::PostResolver
   end
 end
