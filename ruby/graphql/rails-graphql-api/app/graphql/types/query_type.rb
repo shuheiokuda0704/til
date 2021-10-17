4c0d0a6a556dc093ff169a32bc2412c26126e339
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-10-17 18:51:30 +0900

    Add Query post

--- ruby/graphql/rails-graphql-api/app/graphql/types/query_type.rb
+++ ruby/graphql/rails-graphql-api/app/graphql/types/query_type.rb
@@ -7,11 +7,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :post, Types::PostType, null: false do
+      description 'Find a post by ID'
+      argument :id, ID, required: true
+    end
+
+    def post (id:)
+      Post.find(id)
     end
   end
 end
