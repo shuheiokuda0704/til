4f8ab1d773c8e05538031fafc595b6f110734ac8
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-04-24 05:43:18 +0900

    bind-attribute

---
+++ vuejs3/bind-attribute/bind-attribute.js
@@ -0,0 +1,9 @@
+const AttributeBinding = {
+  data() {
+    return {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  }
+}
+
+Vue.createApp(AttributeBinding).mount('#bind-attribute')
